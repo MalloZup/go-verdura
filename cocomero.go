b5ae12422c352d7f56ba19fbef124f20735babf3
dmaiocchi <dmaiocchi@suse.com>
2017-02-21 15:56:58 +0100

    Implement first function for getting results

--- cocomero.go
+++ cocomero.go
@@ -1,22 +1,29 @@
 package main
 
 import (
-       "fmt"
-       "os/exec"
-       "log"
+	"fmt"
+	"log"
+	"os/exec"
 )
 
+// this function get the latest cucumber result
+func getHeadOutput(url string) {
+	cmd := exec.Command("wget", url, "-O", "last.html")
+	stdoutStderr, err := cmd.CombinedOutput()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("%s\n", stdoutStderr)
+}
 
-func getHeadOutput() {
-        cmd := exec.Command("uptime")
-        stdoutStderr, err := cmd.CombinedOutput()
-        if err != nil {
-                log.Fatal(err)
-        }
-        fmt.Printf("%s\n", stdoutStderr)
-
+// get name of failed steps
+func get_failed_steps() {
+	fmt.Printf("\n NOT yet implemented.")
 }
 
 func main() {
-  getHeadOutput()
+	// we can assume that this is constant.
+	const url_head = "http://10.162.210.226/workspace/manager-Head-sumaform-cucumber/last.html"
+	getHeadOutput(url_head)
+	get_failed_steps()
 }
